fae67f634fafc73a3f9b84fc26bb55c83e34ae17
FloatingMilkshake <floatingmilkshake@outlook.com>
2021-09-10 18:45:22 -0400

    remove !pwsh
    i don't really need it and the bot will be running primarily on Docker anyway. the command was really just to see if i could do it

--- Modules/Owner.cs
+++ Modules/Owner.cs
@@ -41,53 +41,5 @@ namespace DiscordBot.Modules
                 await ctx.RespondAsync("Are you sure?");
             }
         }
-
-        [Command("pwsh")]
-        [Description("**Owner only:** Runs a PowerShell command on the host machine.")]
-        public async Task pwsh(CommandContext ctx, [Description("The PowerShell command to run."), RemainingText] String args)
-        {
-            if (ctx.Message.Author.Username != "FloatingMilkshake" && ctx.Message.Author.Discriminator != "7777")
-            {
-                await ctx.RespondAsync("You don't have permission to perform this command!\n`pwsh` is an owner-only command.");
-                return;
-            }
-            var msg = await ctx.Channel.SendMessageAsync($"*Executing...*");
-
-            var pwshTemp = "discordBotTemp.ps1";
-
-            await File.WriteAllTextAsync(pwshTemp, args);
-
-            var start = new ProcessStartInfo
-            {
-                FileName = "pwsh.exe",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                Arguments = pwshTemp,
-                CreateNoWindow = true
-            };
-
-            var process = Process.Start(start);
-            var reader = process.StandardOutput;
-
-            process.EnableRaisingEvents = true;
-
-            var output = reader.ReadToEnd();
-
-            await process.WaitForExitAsync();
-
-            if (output.Contains("is not recognized as a name of a cmdlet, function, script file, or executable"))
-            {
-                output = "The command you entered was not recognized. Try again!";
-            }
-
-            if (output is null or "")
-            {
-                output = "There was no output for this command.";
-            }
-
-            await msg.ModifyAsync($"```\n{output}\n```");
-
-            File.Delete(pwshTemp);
-        }
     }
 }
