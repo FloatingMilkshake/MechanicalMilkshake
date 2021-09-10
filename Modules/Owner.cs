53d79372dbe67a90d126b37bc08ddeaa25f12265
FloatingMilkshake <floatingmilkshake@outlook.com>
2021-09-10 19:13:27 -0400

    make Owner.cs dynamic
    it was previously tied to just my name which doesn't work if someone else was to host the bot. for now i'm using `[RequireOwner]`, was going to just make it require the Administrator permission but i can't quite figure out how to do that, maybe i'll figure it out later

--- Modules/Owner.cs
+++ Modules/Owner.cs
@@ -2,8 +2,6 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using System;
 using System.Threading.Tasks;
-using System.Diagnostics;
-using System.IO;
 
 namespace DiscordBot.Modules
 {
@@ -11,26 +9,18 @@ namespace DiscordBot.Modules
     {
         [Command("tellraw")]
         [Description("**Owner only:** Speak through the bot!")]
+        [RequireOwner]
         public async Task Tellraw(CommandContext ctx, [Description("The message to have the bot send."), RemainingText] string message)
         {
-            if (ctx.Message.Author.Username != "FloatingMilkshake" && ctx.Message.Author.Discriminator != "7777")
-            {
-                await ctx.RespondAsync("You don't have permission to perform this command!\n`tellraw` is an owner-only command.");
-                return;
-            }
             await ctx.Message.DeleteAsync();
             await ctx.Channel.SendMessageAsync(message);
         }
 
         [Command("shutdown")]
         [Description("**Owner only:** Shuts down the bot.")]
+        [RequireOwner]
         public async Task Shutdown(CommandContext ctx, [Description("This must be \"I am sure\" for the command to run."), RemainingText] string areYouSure)
         {
-            if (ctx.Message.Author.Username != "FloatingMilkshake" && ctx.Message.Author.Discriminator != "7777")
-            {
-                await ctx.RespondAsync("You don't have permission to perform this command!\n`shutdown` is an owner-only command.");
-                return;
-            }
             if (areYouSure == "I am sure")
             {
                 await ctx.RespondAsync("**Warning**: The bot is now shutting down.");
