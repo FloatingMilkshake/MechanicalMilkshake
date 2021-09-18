4c7ea98927c82a27b43e20e99286dceff6e5f35b
FloatingMilkshake <floatingmilkshake@outlook.com>
2021-09-18 17:45:48 -0400

    attempt to add command for cloudflare worker-links

--- Modules/Owner.cs
+++ Modules/Owner.cs
@@ -2,6 +2,7 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Modules
@@ -9,7 +10,7 @@ namespace DiscordBot.Modules
     public class Owner : BaseCommandModule
     {
         [Command("shutdown")]
-        [Description("**Owner-only:** Shuts down the bot.")]
+        [Description("Shuts down the bot.")]
         [RequireOwner]
         public async Task Shutdown(CommandContext ctx, [Description("This must be \"I am sure\" for the command to run."), RemainingText] string areYouSure)
         {
@@ -24,5 +25,61 @@ namespace DiscordBot.Modules
                 await ctx.RespondAsync("Are you sure?");
             }
         }
+
+        [Command("link")]
+        public async Task Link(CommandContext ctx, string key, string url)
+        {
+
+            string baseUrl;
+            if (Environment.GetEnvironmentVariable("WORKER_LINKS_BASE_URL") == null)
+            {
+                await ctx.RespondAsync("Error: No base URL provided! Make sure the environment variable `WORKER_LINKS_BASE_URL` is set.");
+                return;
+            }
+            else
+            {
+                baseUrl = Environment.GetEnvironmentVariable("WORKER_LINKS_BASE_URL");
+            }
+
+            using HttpClient httpClient = new()
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+
+            HttpRequestMessage request;
+
+            if (key == null || key == "random")
+            {
+                request = new HttpRequestMessage(HttpMethod.Post, "") { };
+            }
+            else
+            {
+                request = new HttpRequestMessage(HttpMethod.Put, key) { };
+            }
+
+            string secret;
+            if (Environment.GetEnvironmentVariable("WORKER_LINKS_SECRET") == null)
+            {
+                await ctx.RespondAsync("Error: No secret provided! Make sure the environment variable `WORKER_LINKS_secret` is set.");
+                return;
+            }
+            else
+            {
+                secret = Environment.GetEnvironmentVariable("WORKER_LINKS_BASE_URL");
+            }
+
+            request.Headers.Add("Authorization", secret);
+            request.Headers.Add("URL", url);
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            int httpStatusCode = (int)response.StatusCode;
+            string httpStatus = response.StatusCode.ToString();
+            string responseText = await response.Content.ReadAsStringAsync();
+            if (responseText.Length > 1940)
+            {
+                await ctx.Channel.SendMessageAsync($"Worker responded with code: `{httpStatusCode}`...but the full response is too long to post here. Think about connecting this to a pastebin-like service.");
+            }
+            await ctx.Channel.SendMessageAsync($"Worker responded with code: `{httpStatusCode}` (`{httpStatus}`)\n```json\n{responseText}\n```");
+        })
     }
 }
