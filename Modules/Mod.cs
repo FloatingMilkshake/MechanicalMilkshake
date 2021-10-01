eb1f2474e64af28378804d5a94798848df0fc6c3
FloatingMilkshake <floatingmilkshake@outlook.com>
2021-10-01 17:30:03 -0400

    delete: oops i forgot a )

--- Modules/Mod.cs
+++ Modules/Mod.cs
@@ -85,7 +85,7 @@ namespace DiscordBot.Modules
             }
             catch (Exception e)
             {
-                var failureMsg = await ctx.RespondAsync($"Something went wrong! See details below.\n\n```\n{e}\n```\n(This message will be automatically deleted in 15 seconds.");
+                var failureMsg = await ctx.RespondAsync($"Something went wrong! See details below.\n\n```\n{e}\n```\n(This message will be automatically deleted in 15 seconds.)");
                 await Task.Delay(15000);
                 await ctx.Channel.DeleteMessageAsync(failureMsg);
             }
