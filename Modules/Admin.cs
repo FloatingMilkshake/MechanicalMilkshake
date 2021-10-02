d952fb8ad77afd3ce3030bd39f473ab41d9803b8
Rick <wrightricky64@gmail.com>
2021-10-02 04:41:04 +0100

    Convert admin commands to slash commands

--- Modules/Admin.cs
+++ Modules/Admin.cs
@@ -1,38 +1,63 @@
 using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
 using System;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Modules
 {
-    public class Admin : BaseCommandModule
+    public class Admin : ApplicationCommandModule
     {
-        [Command("tellraw")]
-        [Description("**Admin-only:** Speak through the bot!")]
-        [RequirePermissions(Permissions.Administrator)]
-        public async Task Tellraw(CommandContext ctx, [Description("The message to have the bot send."), RemainingText] string message)
+        [SlashCommand("tellraw", "**Admin-only:** Speak through the bot!")]
+        [SlashRequireUserPermissions(Permissions.Administrator)]
+        public async Task Tellraw(InteractionContext ctx, [Option("message", "What do you wish to say?")] string message)
         {
-            await ctx.Message.DeleteAsync();
-            await ctx.Channel.SendMessageAsync(message);
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(message));
         }
 
-        [Command("restart")]
-        [Description("**Admin-only:** Restarts the bot.")]
-        [RequirePermissions(Permissions.Administrator)]
-        public async Task Restart(CommandContext ctx)
+        [SlashCommand("restart", "**Admin-only:** Restarts the bot.")]
+        [SlashRequireOwner] /* Restarting the bot should be only possible for the bot owner. */
+        public async Task Restart(InteractionContext ctx)
         {
-            string dockerCheckFile = File.ReadAllText("/proc/self/cgroup");
-            if (string.IsNullOrWhiteSpace(dockerCheckFile))
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+            var yesButton = new DiscordButtonComponent(ButtonStyle.Danger, "yes", "Yes", false, new DiscordComponentEmoji(":white_check_mark:"));
+            var noButton = new DiscordButtonComponent(ButtonStyle.Danger, "no", "No", false, new DiscordComponentEmoji(":negative_squared_check_mark:"));
+
+            string checkDockerFile = await File.ReadAllTextAsync("/proc/self/cgroup");
+            if (string.IsNullOrWhiteSpace(checkDockerFile))
             {
-                await ctx.RespondAsync("The bot may not be running under Docker; this means that `!restart` will behave like `!shutdown`."
-                    + "\n\nAborted. Use `!shutdown` if you wish to shut down the bot.");
-                return;
-            }
+                var sentMessage = await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("The bot may not be running under Docker; this means that `!restart` will behave like `!shutdown`.\n\nAborted. Do you wish to shut the bot down?").AddComponents(yesButton, noButton));
 
-            await ctx.RespondAsync("Restarting...");
-            Environment.Exit(1);
+                var waitForReaction = await Bot.InteractivityExtension.WaitForButtonAsync(sentMessage, ctx.User);
+
+                if (waitForReaction.Result.Interaction.Data.CustomId.Equals(yesButton)) 
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Shutting down.."));
+                    Environment.Exit(1);
+                }
+            }
+            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Restarting.."));
         }
+
+        //[Command("restart")]
+        //[Description("**Admin-only:** Restarts the bot.")]
+        //[RequirePermissions(Permissions.Administrator)]
+        //public async Task Restart(CommandContext ctx)
+        //{
+        //    string dockerCheckFile = File.ReadAllText("/proc/self/cgroup");
+        //    if (string.IsNullOrWhiteSpace(dockerCheckFile))
+        //    {
+        //        await ctx.RespondAsync("The bot may not be running under Docker; this means that `!restart` will behave like `!shutdown`."
+        //            + "\n\nAborted. Use `!shutdown` if you wish to shut down the bot.");
+        //        return;
+        //    }
+
+        //    await ctx.RespondAsync("Restarting...");
+        //    Environment.Exit(1);
+        //}
     }
 }
