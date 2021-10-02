3c6fab92e6e868d0c91df3a7d224ebcadf9ce8c5
Rick <wrightricky64@gmail.com>
2021-10-02 03:53:45 +0100

    Move event handling to it's own separate file to try to keep the main logic as seemless as possible.

---
+++ Events/CommandEvents.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+namespace DiscordBot.Events
+{
+    public class CommandEvents
+    {
+        public static async Task CommandsNextService_CommandErrored(CommandsNextExtension cnext, CommandErrorEventArgs e)
+        {
+            if (e.Exception is CommandNotFoundException && (e.Command == null || e.Command.QualifiedName != "help"))
+                return;
+
+            var exs = new List<Exception>();
+            if (e.Exception is AggregateException ae)
+                exs.AddRange(ae.InnerExceptions);
+            else
+                exs.Add(e.Exception);
+
+            foreach (var ex in exs)
+            {
+                if (ex is CommandNotFoundException && (e.Command == null || e.Command.QualifiedName != "help"))
+                    return;
+
+                if (ex is ChecksFailedException && (e.Command.Name != "help"))
+                    return;
+
+                var embed = new DiscordEmbedBuilder
+                {
+                    Color = new DiscordColor("#FF0000"),
+                    Title = "An exception occurred when executing a command",
+                    Description = $"`{e.Exception.GetType()}` occurred when executing `{e.Command.QualifiedName}`.",
+                    Timestamp = DateTime.UtcNow
+                };
+                embed.AddField("Message", ex.Message);
+                if (e.Exception.GetType().ToString() == "System.ArgumentException")
+                    embed.AddField("Note", "This usually means that you used the command incorrectly.\n" +
+                        "Please double-check how to use this command.");
+                await e.Context.RespondAsync(embed: embed.Build()).ConfigureAwait(false);
+            }
+        }
+    }
+
+    public class SlashCommandEvents
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -58,42 +58,8 @@ namespace DiscordBot
                 configJson.S3_Region
             ).WithSSL();
 
-            async Task CommandsNextService_CommandErrored(CommandsNextExtension cnext, CommandErrorEventArgs e)
-            {
-                if (e.Exception is CommandNotFoundException && (e.Command == null || e.Command.QualifiedName != "help"))
-                    return;
-
-                var exs = new List<Exception>();
-                if (e.Exception is AggregateException ae)
-                    exs.AddRange(ae.InnerExceptions);
-                else
-                    exs.Add(e.Exception);
-
-                foreach (var ex in exs)
-                {
-                    if (ex is CommandNotFoundException && (e.Command == null || e.Command.QualifiedName != "help"))
-                        return;
-
-                    if (ex is ChecksFailedException && (e.Command.Name != "help"))
-                        return;
-
-                    var embed = new DiscordEmbedBuilder
-                    {
-                        Color = new DiscordColor("#FF0000"),
-                        Title = "An exception occurred when executing a command",
-                        Description = $"`{e.Exception.GetType()}` occurred when executing `{e.Command.QualifiedName}`.",
-                        Timestamp = DateTime.UtcNow
-                    };
-                    embed.AddField("Message", ex.Message);
-                    if (e.Exception.GetType().ToString() == "System.ArgumentException")
-                        embed.AddField("Note", "This usually means that you used the command incorrectly.\n" +
-                            "Please double-check how to use this command.");
-                    await e.Context.RespondAsync(embed: embed.Build()).ConfigureAwait(false);
-                }
-            }
-
             commands.RegisterCommands(Assembly.GetExecutingAssembly());
-            commands.CommandErrored += CommandsNextService_CommandErrored;
+            commands.CommandErrored += Events.CommandEvents.CommandsNextService_CommandErrored;
 
             if (configJson.HomeChannel == null)
             {
