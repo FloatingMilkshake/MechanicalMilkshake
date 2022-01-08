25c92660d00e5251108e80c4d45e4631ece84763
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-01-08 13:48:04 -0500

    tellraw: workaround to make targetChannel optional, add more try/catches

--- Modules/Mod.cs
+++ Modules/Mod.cs
@@ -12,30 +12,123 @@ namespace MechanicalMilkshake.Modules
         [Command("tellraw")]
         [Description("Speak through the bot! Requires the `Kick Members` permission.")]
         [RequireUserPermissions(Permissions.KickMembers)]
-        public async Task Tellraw(CommandContext ctx, [Description("The channel to send the message in.")] DiscordChannel targetChannel, [Description("The message to have the bot send."), RemainingText] string message)
+        public async Task Tellraw(CommandContext ctx, [Description("The message to have the bot send. **If you'd like the message to be sent in a different channel than the one you're sending this command in, put the channel first, followed by a space.**"), RemainingText] string message)
         {
-            try
+            DiscordChannel targetChannel = default;
+            string extractedChannel = null;
+
+            if (message.Contains(' '))
             {
-                await targetChannel.SendMessageAsync(message);
-            }
-            catch (DSharpPlus.Exceptions.UnauthorizedException)
-            {
-                await ctx.RespondAsync("I don't have permission to send messages in that channel!");
-                return;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Exception {e.Message} occurred when {ctx.User.Id} ran {ctx.Command.Name} in #{ctx.Channel.Name} (target channel: {targetChannel.Name})!\nException Details: {e}");
-                await ctx.RespondAsync("Something went wrong when attempting to send that message! This error has been logged.");
-                return;
-            }
-            if (ctx.Channel.IsPrivate)
-            {
-                await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":white_check_mark:"));
+                extractedChannel = message[..message.IndexOf(" ")];
+                if (extractedChannel.Contains("<#"))
+                {
+                    extractedChannel = extractedChannel.Replace("<#", "");
+                }
+                if (extractedChannel.Contains('>'))
+                {
+                    extractedChannel = extractedChannel.Replace(">", "");
+                }
+
+                ulong targetChannelId = default;
+                try
+                {
+                    targetChannelId = Convert.ToUInt64(extractedChannel);
+                }
+                catch
+                {
+                    await ctx.RespondAsync("Hmm, something went wrong trying to read the target channel from your message! Please try again or contact the bot developer.");
+                    return;
+                }
+
+                targetChannel = await ctx.Client.GetChannelAsync(targetChannelId);
+
+                message = message.Replace(extractedChannel, "");
+                if (message.Contains("<#"))
+                {
+                    message = message.Replace("<#", "");
+                }
+                if (message.Contains('>'))
+                {
+                    message = message.Replace(">", "");
+                }
             }
             else
             {
-                await ctx.RespondAsync($"I sent your message to {targetChannel.Mention}.");
+                targetChannel = ctx.Channel;
+            }
+
+            if (targetChannel != ctx.Channel)
+            {
+                try
+                {
+                    await targetChannel.SendMessageAsync(message);
+                }
+                catch (DSharpPlus.Exceptions.UnauthorizedException)
+                {
+                    await ctx.RespondAsync("I don't have permission to send messages in that channel!");
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception {e.Message} occurred when {ctx.User.Id} ran {ctx.Command.Name} in #{ctx.Channel.Name} (target channel: {targetChannel.Name})!\nException Details: {e}");
+                    await ctx.RespondAsync("Something went wrong when attempting to send that message! This error has been logged.");
+                    return;
+                }
+                if (ctx.Channel.IsPrivate)
+                {
+                    await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":white_check_mark:"));
+                }
+                else
+                {
+                    try
+                    {
+                        await ctx.RespondAsync($"I sent your message to {targetChannel.Mention}.");
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":white_check_mark:"));
+                        }
+                        catch
+                        {
+                            // Do nothing because this isn't really important
+                        }
+                    }
+                }
+            }
+            else // if target channel is the same channel in which the command was invoked
+            {
+                try
+                {
+                    await ctx.Message.DeleteAsync();
+                }
+                catch
+                {
+                    // Do nothing; this isn't really a big deal. Maybe this will be changed later.
+                }
+                try
+                {
+                    await targetChannel.SendMessageAsync(message);
+                }
+                catch (DSharpPlus.Exceptions.UnauthorizedException)
+                {
+                    try
+                    {
+                        await ctx.Message.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":x:"));
+                    }
+                    catch
+                    {
+                        // Something went wrong while trying to send the message AND add the reaction. For now we're just going to do nothing. This may be changed later because silently failing isn't great.
+                    }
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception {e.Message} occurred when {ctx.User.Id} ran {ctx.Command.Name} in #{ctx.Channel.Name} (target channel: {targetChannel.Name})!\nException Details: {e}");
+                    await ctx.RespondAsync("Something went wrong when attempting to send that message! This error has been logged.");
+                    return;
+                }
             }
         }
 
