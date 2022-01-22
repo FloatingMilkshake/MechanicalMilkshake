644a274debc4a97c78fee5649e97e9d8329cf8d3
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-01-22 15:05:50 -0500

    improve /timestamp

--- Modules/Utility.cs
+++ Modules/Utility.cs
@@ -139,10 +139,19 @@ namespace MechanicalMilkshake.Modules
         class TimestampCmds : ApplicationCommandModule
         {
             [SlashCommand("id", "Returns the Unix timestamp of a given Discord ID/snowflake.")]
-            public async Task TimestampSnowflakeCmd(InteractionContext ctx, [Option("snowflake", "The ID/snowflake to fetch the Unix timestamp for.")] string id, [Option("format", "The format to convert the timestamp to. Options are F/D/T/R/f/d/t.")] string format = null)
+            public async Task TimestampSnowflakeCmd(InteractionContext ctx, [Option("snowflake", "The ID/snowflake to fetch the Unix timestamp for.")] string id,
+                [Choice("Short Time", "t")]
+                [Choice("Long Time", "T")]
+                [Choice("Short Date", "d")]
+                [Choice("Long Date", "D")]
+                [Choice("Short Date/Time", "f")]
+                [Choice("Long Date/Time", "F")]
+                [Choice("Relative Time", "R")]
+                [Choice("Raw Timestamp", "")]
+                [Option("format", "The format to convert the timestamp to.")] string format = "",
+                [Option("includecode", "Whether to include the code for the timestamp.")] bool includeCode = false)
             {
-                ulong snowflake = default;
-
+                ulong snowflake;
                 try
                 {
                     snowflake = Convert.ToUInt64(id);
@@ -155,30 +164,53 @@ namespace MechanicalMilkshake.Modules
 
                 ulong msSinceEpoch = snowflake >> 22;
                 ulong msUnix = msSinceEpoch + 1420070400000;
-                if (format == null)
+                if (string.IsNullOrWhiteSpace(format))
                 {
                     await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"{msUnix / 1000}"));
                 }
                 else
                 {
-                    string[] validFormats = { "F", "f", "T", "t", "D", "d", "R" };
-                    if (validFormats.Any(format.Contains))
+                    if (includeCode)
                     {
-                        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"<t:{msUnix / 1000}:{format}>"));
+                        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"<t:{msUnix / 1000}:{format}> (`<t:{msUnix / 1000}:{format}>`)"));
                     }
                     else
                     {
-                        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"Hmm, that doesn't look like a valid format. Here's the raw Unix timestamp: {msUnix / 1000}"));
+                        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"<t:{msUnix / 1000}:{format}>"));
                     }
                 }
             }
 
             [SlashCommand("date", "Returns the Unix timestamp of a given date.")]
-            public async Task TimestampDateCmd(InteractionContext ctx, [Option("date", "The date to fetch the Unix timestamp for.")] string date)
+            public async Task TimestampDateCmd(InteractionContext ctx, [Option("date", "The date to fetch the Unix timestamp for.")] string date,
+                [Choice("Short Time", "t")]
+                [Choice("Long Time", "T")]
+                [Choice("Short Date", "d")]
+                [Choice("Long Date", "D")]
+                [Choice("Short Date/Time", "f")]
+                [Choice("Long Date/Time", "F")]
+                [Choice("Relative Time", "R")]
+                [Choice("Raw Timestamp", "")]
+                [Option("format", "The format to convert the timestamp to. Options are F/D/T/R/f/d/t.")] string format = "",
+                [Option("includecode", "Whether to include the code for the timestamp.")] bool includeCode = false)
             {
                 DateTime dateToConvert = Convert.ToDateTime(date);
                 long unixTime = ((DateTimeOffset)dateToConvert).ToUnixTimeSeconds();
-                await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"{unixTime}"));
+                if (string.IsNullOrWhiteSpace(format))
+                {
+                    await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"{unixTime}"));
+                }
+                else
+                {
+                    if (includeCode)
+                    {
+                        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"<t:{unixTime}:{format}> (`<t:{unixTime}:{format}>`)"));
+                    }
+                    else
+                    {
+                        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"<t:{unixTime}:{format}>"));
+                    }
+                }
             }
         }
 
