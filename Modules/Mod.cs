0c338d051475c942a36bb61451a7baeba39e4679
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-06-14 16:24:46 -0400

    add /timeout

--- Modules/Mod.cs
+++ Modules/Mod.cs
@@ -118,6 +118,94 @@ namespace MechanicalMilkshake.Modules
             await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent($"Successfully unbanned **{userToUnban.Username}#{userToUnban.Discriminator}**!"));
         }
 
+        [SlashCommandGroup("timeout", "Set or clear a timeout for a user. Requires the Timeout Members permission.")]
+        [SlashRequirePermissions(Permissions.ModerateMembers)]
+        [SlashRequireGuild]
+        public class TimeoutCmds
+        {
+            [SlashCommand("set", "Time out a user. Requires the Timeout Members permission.")]
+            public async Task SetTimeout(InteractionContext ctx, [Option("member", "The member to time out.")] DiscordUser user, [Option("duration", "How long the timeout should last. Maximum value is 28 days due to Discord limitations.")] string duration, [Option("reason", "The reason for the timeout.")] string reason = "No reason provided.")
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true));
+
+                DiscordMember member;
+
+                try
+                {
+                    member = await ctx.Guild.GetMemberAsync(user.Id);
+                }
+                catch
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Hmm. It doesn't look like that user is in the server, so I can't time them out.").AsEphemeral(true));
+                    return;
+                }
+
+                TimeSpan parsedDuration = HumanDateParser.HumanDateParser.Parse(duration).Subtract(ctx.Interaction.CreationTimestamp.DateTime);
+                DateTime expireTime = ctx.Interaction.CreationTimestamp.DateTime + parsedDuration;
+
+                try
+                {
+                    await member.TimeoutAsync(expireTime, reason);
+                }
+                catch (DSharpPlus.Exceptions.BadRequestException)
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("It looks like you tried to set the timeout duration to more than 28 days in the future! Due to Discord limitations, timeouts can only be up to 28 days.").AsEphemeral(true));
+                    return;
+                }
+                catch (DSharpPlus.Exceptions.UnauthorizedException)
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Something went wrong. You or I may not be allowed to time out **{user.Username}#{user.Discriminator}**! Please check the role hierarchy and permissions.").AsEphemeral(true));
+                    return;
+                }
+                catch (Exception e)
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Hmm, something went wrong while trying to time out that user!\n\nThis was Discord's response:\n> {e.Message}\n\nIf you'd like to contact the bot owner about this, include this debug info:\n```{e}\n```").AsEphemeral(true));
+                    return;
+                }
+
+                DateTime dateToConvert = Convert.ToDateTime(expireTime);
+                long unixTime = ((DateTimeOffset)dateToConvert).ToUnixTimeSeconds();
+
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Successfully timed out {user.Mention} until `{expireTime}` (<t:{unixTime}:R>)!").AsEphemeral(true));
+                await ctx.Channel.SendMessageAsync($"{user.Mention} has been timed out, expiring <t:{unixTime}:R>: **{reason}**");
+            }
+
+            [SlashCommand("clear", "Clear a timeout before it's set to expire. Requires the Timeout Members permission.")]
+            public async Task ClearTimeout(InteractionContext ctx, [Option("member", "The member whose timeout to clear.")] DiscordUser user)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder());
+
+                DiscordMember member;
+
+                try
+                {
+                    member = await ctx.Guild.GetMemberAsync(user.Id);
+                }
+                catch
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Hmm. It doesn't look like that user is in the server, so I can't remove their timeout."));
+                    return;
+                }
+
+                try
+                {
+                    await member.TimeoutAsync(null, "Timeout cleared manually.");
+                }
+                catch (DSharpPlus.Exceptions.UnauthorizedException)
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Something went wrong. You or I may not be allowed to clear the timeout for **{user.Username}#{user.Discriminator}**! Please check the role hierarchy and permissions."));
+                    return;
+                }
+                catch (Exception e)
+                {
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Hmm, something went wrong while trying to clear the timeout for that user!\n\nThis was Discord's response:\n> {e.Message}\n\nIf you'd like to contact the bot owner about this, include this debug info:\n```{e}\n```").AsEphemeral(true));
+                    return;
+                }
+
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent($"Successfully cleared the timeout for {user.Mention}!"));
+            }
+        }
+
         [SlashCommand("nickname", "Changes my nickname. Requires that you have the Manage Nicknames or Manage Server permissions.")]
         [SlashRequireGuild]
         public async Task Nickname(InteractionContext ctx, [Option("nickname", "What to change my nickname to. Leave this blank to clear it.")] string nickname = null)
