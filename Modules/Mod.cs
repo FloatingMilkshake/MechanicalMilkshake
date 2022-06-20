9dd21a6e952f81374a47c862047e41ad488158d3
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-06-20 00:08:54 -0400

    add /lockdown commands

--- Modules/Mod.cs
+++ Modules/Mod.cs
@@ -4,6 +4,80 @@ namespace MechanicalMilkshake.Modules
 {
     public class Mod : ApplicationCommandModule
     {
+        [SlashCommandGroup("lockdown", "Lock or unlock a channel. Requires the Timeout Members permission.")]
+        [SlashRequirePermissions(Permissions.ModerateMembers)]
+        public class Lockdown
+        {
+            [SlashCommand("lock", "Lock a channel to prevent members from sending messages. Requires the Timeout Members permission.")]
+            public async Task Lock(InteractionContext ctx)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true));
+
+                var existingOverwrites = ctx.Channel.PermissionOverwrites.ToArray();
+
+                await ctx.Channel.AddOverwriteAsync(ctx.Member, Permissions.SendMessages, Permissions.None);
+                await ctx.Channel.AddOverwriteAsync(await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id), Permissions.SendMessages, Permissions.None);
+                await ctx.Channel.AddOverwriteAsync(ctx.Guild.EveryoneRole, Permissions.None, Permissions.SendMessages);
+
+                foreach (var overwrite in existingOverwrites)
+                {
+                    if (overwrite.Type == OverwriteType.Role)
+                    {
+                        if (await overwrite.GetRoleAsync() == ctx.Guild.EveryoneRole)
+                        {
+                            await ctx.Channel.AddOverwriteAsync(await overwrite.GetRoleAsync(), overwrite.Allowed, Permissions.SendMessages | overwrite.Denied);
+                        }
+                        else
+                        {
+                            await ctx.Channel.AddOverwriteAsync(await overwrite.GetRoleAsync(), overwrite.Allowed, overwrite.Denied);
+
+                        }
+                    }
+                    else
+                    {
+                        await ctx.Channel.AddOverwriteAsync(await overwrite.GetMemberAsync(), overwrite.Allowed, overwrite.Denied);
+                    }
+                }
+
+                await ctx.Channel.SendMessageAsync("This channel has been locked.");
+
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Channel locked successfully.").AsEphemeral(true));
+            }
+
+            [SlashCommand("unlock", "Unlock a locked channel to allow members to send messages. Requires the Timeout Members permission.")]
+            public async Task Unlock(InteractionContext ctx)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true));
+
+                foreach (var permission in ctx.Channel.PermissionOverwrites.ToArray())
+                {
+                    if (permission.Type == OverwriteType.Role)
+                    {
+                        if (await permission.GetRoleAsync() == ctx.Guild.EveryoneRole && permission.Denied.HasPermission(Permissions.SendMessages))
+                        {
+                            DiscordOverwriteBuilder newOverwrite = new(ctx.Guild.EveryoneRole)
+                            {
+                                Allowed = permission.Allowed,
+                                Denied = (Permissions)(permission.Denied - Permissions.SendMessages)
+                            };
+
+                            await ctx.Channel.AddOverwriteAsync(ctx.Guild.EveryoneRole, newOverwrite.Allowed, newOverwrite.Denied);
+                        }
+                    }
+                    else
+                    {
+                        if (await permission.GetMemberAsync() == await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id) || await permission.GetMemberAsync() == ctx.Member)
+                        {
+                            await permission.DeleteAsync();
+                        }
+                    }
+                }
+
+                await ctx.Channel.SendMessageAsync("This channel has been unlocked.");
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Channel unlocked successfully.").AsEphemeral(true));
+            }
+        }
+
         [SlashCommand("tellraw", "Speak through the bot! Requires the Kick Members, Ban Members or Timeout Members permissions.")]
         public async Task Tellraw(InteractionContext ctx, [Option("message", "The message to have the bot send.")] string message, [Option("channel", "The channel to send the message in.")] DiscordChannel channel = null)
         {
