c311111897d4eae67fbe55179c22235eaf9a659d
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-08-11 22:26:28 -0400

    add delay before running scheduled tasks to let bot connect

--- Program.cs
+++ Program.cs
@@ -209,6 +209,9 @@ internal class Program
 
         // Run checks
 
+        // Delay to give bot time to connect
+        await Task.Delay(1000);
+
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         Task.Run(async () =>
         {
