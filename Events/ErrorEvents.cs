36d1034cb87024187656aa58d0ba60ac46e2547a
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-09-09 22:38:35 -0400

    add slash command registration failure handling

--- Events/ErrorEvents.cs
+++ Events/ErrorEvents.cs
@@ -4,9 +4,9 @@ public class ErrorEvents
 {
     public static async Task SlashCommandErrored(SlashCommandsExtension scmds, SlashCommandErrorEventArgs e)
     {
-        if (e.Exception is SlashExecutionChecksFailedException exception)
+        if (e.Exception is SlashExecutionChecksFailedException execChecksFailedEx)
         {
-            if (exception.FailedChecks.OfType<SlashRequireGuildAttribute>().Any())
+            if (execChecksFailedEx.FailedChecks.OfType<SlashRequireGuildAttribute>().Any())
             {
                 await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
                     new DiscordInteractionResponseBuilder()
@@ -21,6 +21,56 @@ public class ErrorEvents
                         "Hmm, it looks like one of the checks for this command failed. Make sure you and I both have the permissions required to use it, and that you're using it properly. Contact the bot owner if you need help or think I messed up.")
                     .AsEphemeral());
             return;
+        }
+
+        if ((e.Exception is InvalidOperationException &&
+             e.Exception.Message == "Slash commands failed to register properly on startup.") ||
+            e.Exception is NullReferenceException)
+        {
+            Exception exception = e.Exception;
+
+            string ownerMention = "";
+            foreach (var owner in Program.discord.CurrentApplication.Owners)
+            {
+                ownerMention += owner.Mention + "\n";
+            }
+
+            DiscordEmbedBuilder embed = new()
+            {
+                Title = "Slash commands failed to register",
+                Description =
+                    $"Slash commands failed to register on bot startup and {e.Context.User.Mention}'s usage of `/{e.Context.CommandName}` failed. Please restart the bot. Details are below.",
+                Color = DiscordColor.Red
+            };
+            embed.AddField("Exception Details",
+                $"{exception.GetType()}: {exception.Message}\n```\n{exception.StackTrace}\n```");
+
+            try
+            {
+                var dockerCheckFile = File.ReadAllText("/proc/self/cgroup");
+                if (string.IsNullOrWhiteSpace(dockerCheckFile))
+                {
+                    await e.Context.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent("It looks like slash commands are having issues! Sorry for the inconvenience. Bot owners have been alerted.").AsEphemeral());
+                    await Program.homeChannel.SendMessageAsync(ownerMention, embed);
+                    return;
+                }
+            }
+            catch
+            {
+                // /proc/self/cgroup could not be found, which means the bot is not running in Docker.
+                await e.Context.CreateResponseAsync(new DiscordInteractionResponseBuilder().WithContent("It looks like slash commands are having issues! Sorry for the inconvenience. Bot owners have been alerted.").AsEphemeral());
+                await Program.homeChannel.SendMessageAsync(ownerMention, embed);
+                return;
+            }
+
+            await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().WithContent(
+                    $"It looks like slash commands are having issues! I'm trying to fix this issue automatically. Please run the command again in a moment! If it still fails and you see this message again, try again later. Sorry for the inconvenience! Bot owners have been alerted.").AsEphemeral());
+
+            embed.Description = $"Slash commands failed to register on bot startup and {e.Context.User.Mention}'s usage of `/{e.Context.CommandName}` failed. **The bot is attempting to restart automatically.** Details are below.";
+            await Program.homeChannel.SendMessageAsync(ownerMention, embed);
+            
+            Environment.Exit(1);
         }
 
         List<Exception> exs = new();
