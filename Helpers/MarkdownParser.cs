cb9a653ee29f20a1f6ede36f48cc295af1dd9035
Jason <void206551@gmail.com>
2022-11-07 14:44:17 -0500

    Created a markdown helper

---
+++ Helpers/MarkdownParser.cs
@@ -0,0 +1,33 @@
+﻿namespace MechanicalMilkshake.Helpers;
+
+public static class MarkdownParser
+{
+    /*
+     * This parses a string and escapes all the markdown characters.
+     */
+    public static string Parse(string input)
+    {
+        var output = input;
+        output = output.Replace("\\", "\\\\");
+        output = output.Replace("`", @"\`");
+        output = output.Replace("*", @"\*");
+        output = output.Replace("_", @"\_");
+        output = output.Replace("~", @"\~");
+        output = output.Replace(">", @"\>");
+        output = output.Replace("[", @"\[");
+        output = output.Replace("]", @"\]");
+        output = output.Replace("(", @"\(");
+        output = output.Replace(")", @"\)");
+        output = output.Replace("#", @"\#");
+        output = output.Replace("+", @"\+");
+        output = output.Replace("-", @"\-");
+        output = output.Replace("=", @"\=");
+        output = output.Replace("|", @"\|");
+        output = output.Replace("{", @"\{");
+        output = output.Replace("}", @"\}");
+        output = output.Replace(".", @"\.");
+        output = output.Replace("!", @"\!");
+        
+        return output;
+    }
+}
