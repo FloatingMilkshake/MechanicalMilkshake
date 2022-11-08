26be904519427956079f0927f31855342868961e
Jason <void206551@gmail.com>
2022-11-08 17:48:21 -0500

    Removed unneeded escapes

--- Helpers/MarkdownParserHelper.cs
+++ Helpers/MarkdownParserHelper.cs
@@ -8,6 +8,14 @@ public static class MarkdownParser
     public static string Parse(string input)
     {
         var output = input;
+        // If the string has a link, skip it.
+        if (output.Contains("]("))
+        {
+            var link = output.Substring(output.IndexOf("](", StringComparison.Ordinal) + 2);
+            link = link.Substring(0, link.IndexOf(")", StringComparison.Ordinal));
+            output = output.Replace(link, "");
+        }
+        
         output = output.Replace("\\", "\\\\");
         output = output.Replace("`", @"\`");
         output = output.Replace("*", @"\*");
@@ -25,8 +33,6 @@ public static class MarkdownParser
         output = output.Replace("|", @"\|");
         output = output.Replace("{", @"\{");
         output = output.Replace("}", @"\}");
-        output = output.Replace(".", @"\.");
-        output = output.Replace("!", @"\!");
         
         return output;
     }
