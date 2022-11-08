c2a57ec143cccadf97208f4d20c0164d75dda2e8
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-11-07 22:28:50 -0500

    keyword tracking NRE fix
    skip adding user/channel to list of typing users if user or channel is null

--- Events/TypingEvent.cs
+++ Events/TypingEvent.cs
@@ -9,6 +9,8 @@ public class TypingEvent
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         Task.Run(async () =>
         {
+            if (e.User is null || e.Channel is null) return; // prevents NREs later
+            
             KeyValuePair<DiscordUser, DiscordChannel> kv = new(e.User, e.Channel);
             TypingUsers.Add(kv);
             await Task.Delay(10000);
