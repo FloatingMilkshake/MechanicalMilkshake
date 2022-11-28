7a3a08e2d3f3809e5417d87988d9d94f61674e23
FloatingMilkshake <floatingmilkshake@outlook.com>
2022-11-27 22:09:56 -0500

    /serverinfo: add counts for categories, bots, humans

--- Commands/ServerInfo.cs
+++ Commands/ServerInfo.cs
@@ -12,18 +12,36 @@ public class ServerInfo : ApplicationCommandModule
 
         var createdAt = $"{IdHelpers.GetCreationTimestamp(ctx.Guild.Id, true)}";
 
+        var categoryCount = ctx.Guild.Channels.Count(channel => channel.Value.Type == ChannelType.Category);
+
         var embed = new DiscordEmbedBuilder()
             .WithColor(Program.BotColor)
             .AddField("Server Owner", $"{ctx.Guild.Owner.Username}#{ctx.Guild.Owner.Discriminator}")
             .AddField("Description", $"{description}")
             .AddField("Created on", $"<t:{createdAt}:F> (<t:{createdAt}:R>)")
-            .AddField("Channels", $"{ctx.Guild.Channels.Count}", true)
-            .AddField("Members", $"{ctx.Guild.MemberCount}", true)
+            .AddField("Channels", $"{ctx.Guild.Channels.Count - categoryCount}", true)
+            .AddField("Categories", $"{categoryCount}", true)
             .AddField("Roles", $"{ctx.Guild.Roles.Count}", true)
+            .AddField("Members (total)", $"{ctx.Guild.MemberCount}", true)
+            .AddField("Bots", "loading... this might take a while", true)
+            .AddField("Humans", "loading... this might take a while", true)
             .WithThumbnail($"{ctx.Guild.IconUrl}")
             .WithFooter($"Server ID: {ctx.Guild.Id}");
 
-        await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder()
-            .WithContent($"Server Info for **{ctx.Guild.Name}**").AddEmbed(embed));
+        var response = new DiscordInteractionResponseBuilder()
+            .WithContent($"Server Info for **{ctx.Guild.Name}**").AddEmbed(embed);
+
+        await ctx.CreateResponseAsync(response);
+
+        var members = await ctx.Guild.GetAllMembersAsync();
+        var botCount = members.Count(member => member.IsBot);
+        var humanCount = ctx.Guild.MemberCount - botCount;
+
+        var newEmbed = response.Embeds[0];
+
+        newEmbed.Fields.FirstOrDefault(field => field.Name == "Bots")!.Value = $"{botCount}";
+        newEmbed.Fields.FirstOrDefault(field => field.Name == "Humans")!.Value = $"{humanCount}";
+
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent(response.Content).AddEmbed(embed));
     }
 }
