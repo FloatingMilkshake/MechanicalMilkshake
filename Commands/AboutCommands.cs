76dfe7005d60a7fd735cac993ca5f2272721707c
FloatingMilkshake <me@floatingmilkshake.com>
2023-05-11 20:53:16 -0400

    update /about embed

--- Commands/AboutCommands.cs
+++ Commands/AboutCommands.cs
@@ -7,53 +7,53 @@ public class AboutCommands : ApplicationCommandModule
     {
         await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
 
-        // Set this to an empty string to disable the Privacy Policy notice in /about, or change it to your own Privacy Policy URL if you have one.
+        // Set this to an empty string to disable the Privacy Policy notice in /about, or change it to your own
+        // Privacy Policy URL if you have one.
         const string privacyPolicyUrl = "https://floatingmilkshake.com/privacy#MechanicalMilkshake";
 
-        // Set this to an empty string to disable the Support Server notice in /about, or change it your own Support Server invite if you have one.
+        // Set this to an empty string to disable the Support Server notice in /about, or change it your own
+        // Support Server invite if you have one.
         const string supportServerInvite = "https://floatingmilkshake.link/bot/support";
 
+        // Create embed
+        // Description
         DiscordEmbedBuilder embed = new()
         {
             Title = $"About {ctx.Client.CurrentUser.Username}",
             Description =
-                $"A multipurpose bot with many miscellaneous commands. Type `/` and select {ctx.Client.CurrentUser.Username} to see available commands.",
+                "Hi! I'm a multipurpose bot that can do a bunch of different stuff. If you want to see a list of"
+                + $" commands, hit `/` and pick me!",
             Color = Program.BotColor
         };
+
+        // Server count, command count
         embed.AddField("Servers", ctx.Client.Guilds.Count.ToString(), true);
         embed.AddField("Commands", Program.ApplicationCommands.Count.ToString(), true);
 
         // Privacy Policy link; hidden if privacyPolicyUrl is empty.
         if (!string.IsNullOrWhiteSpace(privacyPolicyUrl))
-            embed.Description += $"\n\nThis bot's Privacy Policy can be found [here]({privacyPolicyUrl}).";
+            embed.Description += $"\n\nMy Privacy Policy can be found [here]({privacyPolicyUrl})!";
 
-        // Support Server link; hidden if supportServerInvite is empty.
-        if (!string.IsNullOrWhiteSpace(supportServerInvite))
-            embed.Description += $"\nNeed help? Join the bot's support server [here]({supportServerInvite})!";
+        // Repo link
+        var remoteUrl = await FileHelpers.ReadFileAsync("RemoteUrl.txt", "");
+        if (remoteUrl != "") embed.AddField("Source Code Repository", remoteUrl);
 
-        // Commit hash / version
-        var commitHash = await FileHelpers.ReadFileAsync("CommitHash.txt", "dev");
-        var remoteUrl = await FileHelpers.ReadFileAsync("RemoteUrl.txt", "N/A");
-        embed.AddField("Source Code Repository", remoteUrl);
-
+        // Bot owner info
         var botOwners = ctx.Client.CurrentApplication.Owners.ToList();
 
         var ownerOutput = botOwners.Count == 1
-            ? $"Bot owner is {botOwners.First().Username}#{botOwners.First().Discriminator}."
+            ? $"The bot owner is {botOwners.First().Username}#{botOwners.First().Discriminator}."
             : botOwners.Aggregate("Bot owners are:",
                 (current, owner) => current + $"\n- {owner.Username}#{owner.Discriminator}");
 
-        ownerOutput = ownerOutput.Trim() +
-                      "\n\nFor any issues with the bot, DM the bot itself or an owner. DMs sent to the bot are forwarded to owners.";
-
         embed.AddField("Owners", ownerOutput);
 
-        var startTime = Convert.ToDateTime(Program.ProcessStartTime);
-        var startUnixTime = ((DateTimeOffset)startTime).ToUnixTimeSeconds();
-        embed.AddField("Uptime", $"Up since <t:{startUnixTime}:F> (<t:{startUnixTime}:R>!)");
-
-        embed.WithFooter(
-            $"Using DSharpPlus {Program.Discord.VersionString} and {RuntimeInformation.FrameworkDescription}\nRunning commit {commitHash}");
+        // Need help?
+        embed.AddField("Need help?",
+            "If you need help with the bot or would like to report an issue, there are a few ways to do so! You can:"
+        + (string.IsNullOrWhiteSpace(supportServerInvite) ? "" : $"\n- Join the bot's [support server]({supportServerInvite})")
+        + $"\n- DM the bot itself (DMs are forwarded to owners!)"
+        + $"\n- DM a bot owner (see above for a list!)");
 
         await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().AddEmbed(embed));
     }
