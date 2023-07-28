2cac926df8ad14ce2a5918a3b57b2c9b09258a83
FloatingMilkshake <me@floatingmilkshake.com>
2023-07-27 21:56:09 -0400

    improvements to emoji commands
    - move /stealemoji and /bigemoji into /emoji get & /emoji enlarge, respectively
    - make /emoji enlarge actually work
    - remove ability to add emoji with /emoji get to avoid ratelimits
    - add ability to export all emoji as zip with /emoji get

--- Commands/EmojiCommands.cs
+++ Commands/EmojiCommands.cs
@@ -1,214 +1,179 @@
-﻿namespace MechanicalMilkshake.Commands;
+﻿using System.IO.Compression;
+
+namespace MechanicalMilkshake.Commands;
 
 public class EmojiCommands : ApplicationCommandModule
 {
     private static readonly Regex EmojiRegex = new(@"<(a)?:([A-z]*):([0-9]*)>");
-    
-    [SlashCommand("stealemoji",
-        "Fetch all of a server's emoji! Note that the bot must be in the server for this to work.")]
-    public static async Task StealEmoji(InteractionContext ctx,
-        [Option("server", "The ID of the server to fetch emoji from.")]
-        string server = "",
-        [Option("add_to_server", "Whether to add all of the emoji to the server you're running the command in.")]
-        bool addToServer = false,
-        [Option("emoji", "The emoji to add to this server. Overrides add_to_server to True.")]
-        string emojiToAdd = "")
+
+    [SlashCommandGroup("emoji", "Commands for working with emoji.")]
+    [SlashCommandPermissions(Permissions.ManageEmojis)]
+    public class Emoji
     {
-        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-
-        if (emojiToAdd != "")
+        [SlashCommand("get", "Get all emoji from a server. The bot must be in the server for this to work.")]
+        public static async Task GetEmoji(InteractionContext ctx,
+            [Option("server", "The ID of the server to get emoji from.")] string server,
+            [Option("zip", "Whether to include a zip file containing all of the emoji. Defaults to True.")] bool zip = true)
         {
-            if (server != "")
+            // Defer interaction response
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+            // Validate guild ID
+            if (server.Any(character => !char.IsNumber(character)))
             {
                 await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                    "You can't use `server` and `emoji` together! Please try again using only one."));
+                    "That doesn't look like a server ID!\n"
+                    + "A server ID is composed of only numbers and looks something like this: `799644062973427743`.\n"
+                    + "Please try again."));
                 return;
             }
 
-            if (!ctx.Member.Permissions.HasPermission(Permissions.ManageEmojis) || !ctx.Guild.CurrentMember.Permissions.HasPermission(Permissions.ManageEmojis))
-            {
-                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                    "Hmm, it looks like you or I may be missing permissions to add emoji in this server! Be sure that we both have permission to manage emoji in this server."));
-                return;
-            }
-
-            var emojiRxMatches = EmojiRegex.Matches(emojiToAdd);
-
-            if (emojiRxMatches[0].Captures.Count < 1)
-            {
-                await ctx.FollowUpAsync(
-                    new DiscordFollowupMessageBuilder().WithContent(
-                        "Hmm, that doesn't look like an emoji! Please try again."));
-                return;
-            }
-
-            // keep list of added emoji for reference later
-            List<DiscordEmoji> addedEmoji = new();
-            
-            foreach (Match match in emojiRxMatches)
-            {
-                // get link
-                emojiToAdd = $"https://cdn.discordapp.com/emojis/{match.Groups[3]}.{(match.Groups[1].ToString() == "a" ? "gif" : "png")}";
-            
-                // add to server
-                try
-                {
-                    var stream = await Program.HttpClient.GetStreamAsync(emojiToAdd);
-                    using MemoryStream ms = new();
-                    await stream.CopyToAsync(ms);
-                    ms.Position = 0;
-                    addedEmoji.Add(await ctx.Guild.CreateEmojiAsync(match.Groups[2].ToString(), ms, null,
-                        $"Emoji added with /{ctx.CommandName} by user {UserInfoHelpers.GetFullUsername(ctx.User)} ({ctx.User.Id})."));
-                }
-                catch (Exception ex)
-                {
-                    await ctx.FollowUpAsync(
-                        new DiscordFollowupMessageBuilder().WithContent(
-                            "I couldn't add that emoji! An unknown error occurred. Please try again."));
-                
-                    // log error to home channel for debugging
-                    await Program.HomeChannel.SendMessageAsync(new DiscordEmbedBuilder()
-                        .WithColor(DiscordColor.Red)
-                        .WithTitle("An error occurred when executing a slash command")
-                        .WithDescription("An unknown error occurred while using `/stealemoji` with the `emoji` argument.")
-                        .AddField("Invoking User", ctx.User.Id.ToString())
-                        .AddField("Emoji URL", emojiToAdd)
-                        .AddField("Target Guild", ctx.Guild.Id.ToString())
-                        .AddField("Bot has Manage Emoji Permission",
-                            ctx.Guild.CurrentMember.Permissions.HasPermission(Permissions.ManageEmojis).ToString())
-                        .AddField("Stack Trace", $"```\n{ex.StackTrace}\n```"));
-                
-                    return;
-                }
-            }
-
-            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(addedEmoji.Aggregate("Done! ",
-                (current, emoji) =>
-                    current + ((emoji != null ? $"<{(emoji.IsAnimated ? "a:" : ":") + emoji.Name}:{emoji.Id}" : "") +
-                               ">"))));
-
-            return;
-        }
-
-        if (server.Any(character => !char.IsNumber(character)))
-        {
-            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                "That doesn't look like a server ID!\nA server ID is composed of only numbers and looks something like this: `799644062973427743`.\nPlease try again."));
-            return;
-        }
-
-        DiscordGuild guild;
-        try
-        {
-            var guildId = Convert.ToUInt64(server);
-            guild = await ctx.Client.GetGuildAsync(guildId);
-        }
-        catch (UnauthorizedException)
-        {
-            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                "I was able to find that server, but I don't have access to its emoji! The most likely reason for this is that I am not in that server; I cannot fetch emoji from a server I am not in." +
-                $"\n\nIf you think I am in the server and you're still seeing this, contact the bot owner for help (if you don't know who that is, see {SlashCmdMentionHelpers.GetSlashCmdMention("about")}!)."));
-            return;
-        }
-        catch
-        {
-            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                $"I couldn't find that server! Make sure `{server}` is a server ID. If you're sure it is and you're still seeing this, contact the bot owner for help."));
-            return;
-        }
-
-        var response = $"Emoji for **{guild.Name}**\n\n";
-
-        List<DiscordEmoji> stolenEmoji = new();
-        List<DiscordEmoji> copiedEmoji = new();
-        List<DiscordEmoji> failedEmoji = new();
-
-        foreach (var emoji in guild.Emojis)
-        {
-            stolenEmoji.Add(emoji.Value);
-
-            if (!addToServer) continue;
-
-            if (ctx.Channel.IsPrivate)
-            {
-                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                    "I can't add emoji when you use this command in DMs! Please use it in the server you want to add the emoji to."));
-                return;
-            }
-
-            if (!ctx.Member.Permissions.HasPermission(Permissions.ManageEmojis) ||
-                !ctx.Guild.CurrentMember.Permissions.HasPermission(Permissions.ManageEmojis))
-            {
-                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
-                    "I couldn't copy these emoji to this server! Make sure you and I both have the \"Manage Emojis and Stickers\" permission."));
-                return;
-            }
-
-            var stream = await Program.HttpClient.GetStreamAsync(emoji.Value.Url);
-            using MemoryStream ms = new();
-            await stream.CopyToAsync(ms);
-            ms.Position = 0;
+            // Fetch guild
+            DiscordGuild guild;
             try
             {
-                await ctx.Guild.CreateEmojiAsync(emoji.Value.Name, ms, null,
-                    $"Emoji copied from guild {guild.Id} with /{ctx.CommandName} by user {UserInfoHelpers.GetFullUsername(ctx.User)} ({ctx.User.Id}).");
+                var guildId = Convert.ToUInt64(server);
+                guild = await ctx.Client.GetGuildAsync(guildId);
+            }
+            catch (UnauthorizedException)
+            {
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
+                    "I was able to find that server, but I don't have access to its emoji! "
+                    + "The most likely reason for this is that I am not in that server; "
+                    + "I cannot fetch emoji from a server I am not in."
+                    + "\n\nIf you think I am in the server and you're still seeing this, contact the bot owner for "
+                    + $"help (if you don't know who that is, see {SlashCmdMentionHelpers.GetSlashCmdMention("about")}!)."));
+                return;
             }
             catch
             {
-                failedEmoji.Add(emoji.Value);
-                continue;
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(
+                    $"I couldn't find that server! Make sure `{server}` is a server ID. "
+                    + "If you're sure it is and you're still seeing this, contact the bot owner for help."));
+                return;
             }
 
-            copiedEmoji.Add(emoji.Value);
+            // Keep a list of found emoji for downloading
+            var foundEmoji = new List<DiscordEmoji>();
+            foreach (DiscordEmoji emoji in guild.Emojis.Values)
+            {
+                foundEmoji.Add(emoji);
+            }
+
+            // Collect emoji and create response
+
+            var response = $"Emoji for **{guild.Name}**\n\n";
+
+            // Static emoji
+            response += "**Static Emoji**\n";
+            response = foundEmoji.Where(emoji => !emoji.IsAnimated)
+                .Aggregate(response, (current, emoji) => current + $"<:{emoji.Name}:{emoji.Id}> ");
+
+            // Animated emoji
+            response += "\n\n**Animated Emoji**\n";
+            response = foundEmoji.Where(emoji => emoji.IsAnimated)
+                .Aggregate(response, (current, emoji) => current + $"<a:{emoji.Name}:{emoji.Id}> ");
+
+            // Create response builder; will be updated to add zip file if necessary, or sent as-is
+            var responseBuilder = new DiscordFollowupMessageBuilder().WithContent(response);
+
+            // Fetch each emoji, download & archive
+            string tempDir = "";
+            FileStream file = default;
+            if (zip)
+            {
+                // Create a temporary directory to store the emoji
+                tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                Directory.CreateDirectory(tempDir);
+
+                var downloadCompleteSuccess = true; // this is bad prove true pls
+
+                // Download each emoji
+                foreach (var emoji in foundEmoji)
+                {
+                    var emojiUrl = emoji.IsAnimated
+                        ? $"https://cdn.discordapp.com/emojis/{emoji.Id}.gif?size=4096"
+                        : $"https://cdn.discordapp.com/emojis/{emoji.Id}.png?size=4096";
+
+                    HttpRequestMessage httpRequest = new(HttpMethod.Get, emojiUrl);
+                    var httpResponse = await Program.HttpClient.SendAsync(httpRequest);
+                    if (!httpResponse.IsSuccessStatusCode)
+                    {
+                        downloadCompleteSuccess = false;
+                        continue;
+                    }
+
+                    var emojiPath = Path.Combine(tempDir, $"{emoji.Name}." + (emoji.IsAnimated ? "gif" : "png"));
+                    await using var emojiStream = await httpResponse.Content.ReadAsStreamAsync();
+                    await using var fileStream = File.Create(emojiPath);
+                    await emojiStream.CopyToAsync(fileStream);
+                }
+
+                var zipPath = Path.Combine(tempDir, $"{guild.Id}.zip");
+
+                // Add each emoji to zip
+                using (FileStream zipStream = File.Create(zipPath))
+                {
+                    ZipArchive zipArchive = new(zipStream, ZipArchiveMode.Create);
+                    foreach (var emoji in foundEmoji)
+                    {
+                        var emojiPath = Path.Combine(tempDir, $"{emoji.Name}." + (emoji.IsAnimated ? "gif" : "png"));
+                        zipArchive.CreateEntryFromFile(emojiPath, $"{emoji.Name}." + (emoji.IsAnimated ? "gif" : "png"));
+                    }
+                }
+
+                // Add zip & note to response
+                response += "\n\nA zip file is attached containing all of the emoji from this server.";
+
+                file = File.OpenRead(zipPath);
+                responseBuilder.AddFile($"{guild.Id}.zip", file);
+
+                // Include note if some emoji failed to download
+                if (!downloadCompleteSuccess)
+                {
+                    response += "\n**Note:** Some emoji failed to download and are not included in the zip file.";
+                }
+
+                // Update response in builder
+                responseBuilder.WithContent(response);
+            }
+
+            await ctx.FollowUpAsync(responseBuilder);
+
+            // Clean up
+            await file.DisposeAsync();
+            Directory.Delete(tempDir, true);
         }
 
-        // Static emoji
-        response += "**Static Emoji**\n";
-        response = stolenEmoji.Where(emoji => !emoji.IsAnimated)
-            .Aggregate(response, (current, emoji) => current + $"<:{emoji.Name}:{emoji.Id}> ");
-
-        // Animated emoji
-        response += "\n\n**Animated Emoji**\n";
-        response = stolenEmoji.Where(emoji => emoji.IsAnimated)
-            .Aggregate(response, (current, emoji) => current + $"<a:{emoji.Name}:{emoji.Id}> ");
-
-        if (addToServer && failedEmoji.Count > 0)
-            // Emoji were copied, but some failed.
-            response += "\n\nThe following emoji were **not** copied to this server due to an unknown error:\n" +
-                        failedEmoji.Aggregate(response,
-                            (current, emoji) =>
-                                current + (emoji.IsAnimated
-                                    ? $"<a:{emoji.Name}:{emoji.Id}> "
-                                    : $"<:{emoji.Name}:{emoji.Id}> "));
-        else if (copiedEmoji.Count > 0) response += "\n\nThese emoji were copied to this server.";
-
-        await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(response));
-    }
-
-    [SlashCommand("bigemoji", "Enlarge an emoji! Only works for custom emoji.")]
-    public static async Task BigEmoji(InteractionContext ctx, [Option("emoji", "The emoji to enlarge.")] string emoji)
-    {
-        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-
-        if (!EmojiRegex.IsMatch(emoji))
+        [SlashCommand("enlarge", "Enlarge an emoji! Only works for custom emoji.")]
+        public static async Task EnlargeEmoji(InteractionContext ctx, [Option("emoji", "The emoji to enlarge.")] string emoji)
         {
-            await ctx.FollowUpAsync(
-                new DiscordFollowupMessageBuilder().WithContent(
-                    "That doesn't look like an emoji! Please try again."));
-            return;
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+            if (!EmojiRegex.IsMatch(emoji))
+            {
+                await ctx.FollowUpAsync(
+                    new DiscordFollowupMessageBuilder().WithContent(
+                        "That doesn't look like an emoji! Please try again."));
+                return;
+            }
+
+            var matches = EmojiRegex.Matches(emoji);
+
+            var response = "";
+            foreach (Match match in matches)
+            {
+                var groups = match.Groups;
+                var emojiUrl = groups[1].Value == "a"
+                ? $"https://cdn.discordapp.com/emojis/{groups[3].Value}.gif"
+                : $"https://cdn.discordapp.com/emojis/{groups[3].Value}.png";
+
+                HttpRequestMessage httpRequest = new(HttpMethod.Get, emojiUrl);
+                var httpResponse = await Program.HttpClient.SendAsync(httpRequest);
+                response += (httpResponse.IsSuccessStatusCode ? emojiUrl : "[This emoji doesn't seem to exist! Please try again...]") + "\n";
+            }
+
+            await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(response));
         }
-
-        var matches = EmojiRegex.Matches(emoji);
-        var groups = matches[0].Groups;
-
-        var emojiUrl = groups[1].Value == "a"
-            ? $"https://cdn.discordapp.com/emojis/{groups[2].Value}.gif"
-            : $"https://cdn.discordapp.com/emojis/{groups[2].Value}";
-
-        HttpRequestMessage httpRequest = new(HttpMethod.Get, emojiUrl);
-        var httpResponse = await Program.HttpClient.SendAsync(httpRequest);
-        var response = httpResponse.IsSuccessStatusCode ? emojiUrl : "That emoji doesn't exist! Please try again.";
-
-        await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(response));
     }
 }
