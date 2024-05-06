f1ca5e3e55cd81ae8cfa0e9e67eba4e23cf65828
FloatingMilkshake <me@floatingmilkshake.com>
2024-05-06 17:42:46 -0400

    update server-specific features

--- ServerSpecificFeatures.cs
+++ ServerSpecificFeatures.cs
@@ -6,35 +6,6 @@ public partial class ServerSpecificFeatures
     {
         public static async Task MessageCreateChecks(MessageCreateEventArgs e)
         {
-            if (!e.Channel.IsPrivate && e.Guild.Id == 1096948488014659728)
-            {
-                // Get member object to fetch roles
-                var member = await e.Guild.GetMemberAsync(e.Author.Id);
-                // Check roles, only continue if member does not have either of two specific roles
-                if (member.Roles.All(role => role.Id != 1099413365090156655 && role.Id != 1099421482783871107))
-                {
-                    // Regex to match mentions
-                    var mentionRegex = MentionPattern();
-
-                    // Check msg content against regex
-                    if (mentionRegex.IsMatch(e.Message.Content))
-                    {
-                        // Get msg before current
-                        var msgBefore = (await e.Channel.GetMessagesBeforeAsync(e.Message.Id, 1))[0];
-
-                        // If msg before also matches pattern, is from same member, has no other content aside from match, and was sent within 30 seconds, time out & warn
-                        if (mentionRegex.IsMatch(msgBefore.Content) && msgBefore.Author.Id == e.Author.Id &&
-                            msgBefore.CreationTimestamp.AddSeconds(30) > e.Message.CreationTimestamp &&
-                            mentionRegex.Matches(msgBefore.Content)[0].Groups[3].Value == string.Empty && mentionRegex.Matches(e.Message.Content)[0].Groups[3].Value == string.Empty &&
-                            mentionRegex.Matches(msgBefore.Content)[0].Groups[1].Value == string.Empty && mentionRegex.Matches(e.Message.Content)[0].Groups[1].Value == string.Empty)
-                        {
-                            await member.ModifyAsync(m => m.CommunicationDisabledUntil = (DateTimeOffset)DateTime.Now.AddMinutes(5));
-                            await e.Message.RespondAsync("no mass pings :3");
-                        }
-                    }
-                }
-            }
-
             if (e.Guild.Id == 799644062973427743)
             {
                 // &caption -> #captions
