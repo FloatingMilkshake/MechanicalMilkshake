b070fd0167a3e06005eac25de2a6415666575676
FloatingMilkshake <me@floatingmilkshake.com>
2025-10-02 15:51:51 -0400

    HTTP commands: Update list of HTTP response codes, tweak not supported error message

--- Commands/HttpApiCommands.cs
+++ Commands/HttpApiCommands.cs
@@ -4,10 +4,10 @@ public class HttpApiCommands
 {
     private static readonly List<int> HttpCodes =
     [
-        100, 101, 102, 200, 201, 202, 203, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402,
-        403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424,
-        425, 426, 429, 431, 444, 450, 451, 497, 498, 499, 500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511,
-        521, 522, 523, 525, 599
+        100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 214, 226, 300, 301, 302, 303, 304, 305,
+        307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418,
+        419, 420, 421, 422, 423, 424, 425, 426, 428, 429, 431, 444, 450, 451, 495, 496, 497, 498, 499, 500, 501,
+        502, 503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525, 530, 599
     ];
 
     [Command("httpcat")]
@@ -57,7 +57,7 @@ public class HttpApiCommands
         {
             if (!HttpCodes.Contains(code.Value))
                 return
-                    $"Either that's not a valid HTTP status code, or it's not supported by http.{tld}! Try another one.";
+                    $"Either that's not a valid HTTP status code, or it's not supported! Try another one.";
 
             var httpResponse =
                 Program.HttpClient.Send(new HttpRequestMessage(HttpMethod.Get, $"https://http.{tld}/{code}.jpg"));
